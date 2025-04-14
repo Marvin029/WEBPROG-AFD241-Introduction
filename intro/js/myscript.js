393656a53a4c1968348caa87f27001853ef9b749
John Marvin Sumalinog <137606551+Marvin029@users.noreply.github.com>
2025-04-14 11:17:02 +0800

    fix

--- intro/js/myscript.js
+++ intro/js/myscript.js
@@ -1,6 +1,5 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
-    
     document.getElementById("p2").style.color = "blue";
     document.getElementById("p2").style.fontFamily = "Arial";
     document.getElementById("p2").style.fontSize = "larger";
